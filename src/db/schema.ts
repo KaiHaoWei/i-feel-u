678ee3a63e20b98da1fe06f017718f093793cdeb
KaiHaoWei <kevin200220931@yahoo.com>
2024-11-22 18:28:07 +0800

    add user schema

---
+++ src/db/schema.ts
@@ -0,0 +1,19 @@
+import { relations } from "drizzle-orm";
+import {
+  index,
+  pgTable,
+  timestamp,
+  uuid,
+  varchar,
+  primaryKey,
+  integer,
+  boolean,
+  jsonb,
+  text,
+} from "drizzle-orm/pg-core";
+
+export const userTable = pgTable("Users", {
+  displayId: uuid("display_id").defaultRandom().notNull().primaryKey(),
+  userEmail: varchar("user_email").notNull(),
+  userPassword: varchar("user_password", { length: 20 }).notNull(),
+});
