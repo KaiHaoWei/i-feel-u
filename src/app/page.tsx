a9d5baf1205e59e311c7fac8cc3a02b5ba58b3be
Kirumi1019 <wobd124d@gmail.com>
2024-11-24 02:38:30 +0800

    deploy fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ import IFeelULogo from "@/../public/IFEELU_Logo_transparent.png";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center min-h-screen  gap-16 sm:p-10 bg-[#F0E7DA]">
-      {/* title box */}
+    <div className="flex flex-col items-center min-h-screen gap-16 sm:p-10 bg-[#F0E7DA]">
+      {/* Title box */}
       <div className="flex items-center bg-white rounded-3xl px-20 py-40 shadow-lg w-full min-w-fit min-h-fit">
         <div className="flex flex-col w-1/3 items-start">
           <h1 className="text-8xl text-[#9a8980] bevan-regular">NEVER</h1>
@@ -14,12 +14,13 @@ export default function Home() {
           <h1 className="text-8xl text-[#9a8980] bevan-regular">FELT?</h1>
         </div>
         <div className="flex w-2/3 justify-end">
-          <h1 className="mr-20 text-9xl text-[#292628] bevan-regular ">I</h1>
-          <h1 className="mr-20 text-9xl text-[#6d5b47] bevan-regular ">FEEL</h1>
+          <h1 className="mr-20 text-9xl text-[#292628] bevan-regular">I</h1>
+          <h1 className="mr-20 text-9xl text-[#6d5b47] bevan-regular">FEEL</h1>
           <h1 className="text-9xl text-[#9a8980] bevan-regular">U</h1>
         </div>
       </div>
-      {/* introduction */}
+
+      {/* Introduction */}
       <div className="flex justify-between items-center px-20 w-full min-w-fit min-h-fit">
         <div className="flex flex-col">
           <h1 className="font-medium text-2xl my-10">
@@ -31,26 +32,25 @@ export default function Home() {
           </h1>
           <div className="flex">
             <Button className="mr-10 p-10 py-8 w-fit rounded-full hover:bg-[#6d5b47] bg-[#9a8980] my-10">
-              <h1 className="text-white font-semibold text-lg">
-                <Link href={`/main/Login`}>進入聊天室</Link>
-              </h1>
+              <Link href="/main/Login">
+                <h1 className="text-white font-semibold text-lg">進入聊天室</h1>
+              </Link>
             </Button>
             <Button className="mr-10 p-10 py-8 w-fit rounded-full hover:bg-[#6d5b47] bg-[#9a8980] my-10">
-              <h1 className="text-white font-semibold text-lg">
-                <Link href={`/main/About`}>關於 I Feel U</Link>
-              </h1>
+              <Link href="/main/About">
+                <h1 className="text-white font-semibold text-lg">關於 I Feel U</h1>
+              </Link>
             </Button>
             <Button className="mr-10 p-10 py-8 w-fit rounded-full hover:bg-[#6d5b47] bg-[#9a8980] my-10">
-              <h1 className="text-white font-semibold text-lg">
-                <Link href={`/main/ContactUs`}>聯絡我們</Link>
-              </h1>
+              <Link href="/main/ContactUs">
+                <h1 className="text-white font-semibold text-lg">聯絡我們</h1>
+              </Link>
             </Button>
           </div>
         </div>
         <div className="w-full max-w-xl">
           <Image
             src={IFeelULogo}
-            // layout="responsive"
             width={1000}
             height={1000}
             alt="I FEEL U 團隊"
